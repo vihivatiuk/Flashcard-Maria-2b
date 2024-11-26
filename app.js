6c80eb7616de3c535f13d49542ab075d61497916
Maria Vitoria <132943819+vihivatiuk@users.noreply.github.com>
2024-11-26 09:08:40 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 function criaCartao(categoria, pergunta, resposta){
-let container = document.getElementById('container')
-let cartao = document.createElement('article')    
-cartao.className = 'cartao'
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
-cartao.innerHTML = `
-   <div class="cartão__conteudo">
-    <h3>${categoria}</h3>
-    <div class="cartão__conteudo__pergunta">
-    <p>${pergunta}</p>
-    </div>
-    <div class="cartão__conteudo__resposta">
-    <p>${resposta}</p> 
-    </div>
-     </div>
-`
-let respostaEstaVisivel = false  
-function viraCartao(){
-    respostaEstaVisivel = !respostaEstaVisivel
-    cartao.classList.toggle('active'),respostaEstaVisivel
-}
+    cartao.innerHTML = `
+        <div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+        </div>
+        </div>
+    `
 
-container.appendChild(cartao)
+    let respostaEstaVisivel = false
+    function viraCartao(){
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active'), respostaEstaVisivel
+    }
 
+    cartao.addEventListener('click', viraCartao)
+    container.appendChild(cartao)
 }
--- perguntas.js
+++ perguntas.js
@@ -1,12 +1,7 @@
-criaCartao(
-    'programação',
-    'O que é javaScript',
-    'O JavaScript é uma linguagem de programação'
-)
 
 criaCartao(
     'pessoal',
-    'Qual é a mnha cor favorita',
+    'Qual é a mnha cor favorita?',
     'minha cor favorita é rosa'
 )
 
